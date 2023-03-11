639a1fa64be397ecb35f593672ec806bff4e9efb
deHasara <hasaramaithreedesilva@gmail.com>
2023-03-10 23:19:07 -0500

    modified

--- scan/scan.h
+++ scan/scan.h
@@ -15,7 +15,7 @@ using namespace parlay;
 template <class T, class F>
 T scan_inplace_serial(T *A, size_t n, const F& f, T id) {
   T cur = id;
-  std::cout << "cur: " << cur << std::endl;
+  //std::cout << "cur: " << cur << std::endl;
 
   for (size_t i=0; i<n; ++i) {
     T next = f(cur, A[i]);
@@ -37,53 +37,51 @@ T scan_inplace(T *A, size_t n, const F& f, T id) {
   return id;  // TODO
 }
  */
-auto MinSize = 100;
 
-template <typename Input, typename Output, typename F>
-Output scan_inplace(Input begin, Input end, Output to, F function){
-    auto n = std::distance(begin, end);
-    if (0<n){
-        to[0] = begin[0];
-        if(1<n){
-            std::vector<std::decay_t<decltype(*begin)>> t(n);
-            scan_up(begin + 1, end, t.begin(), function);
-            scan_down(begin[0], begin + 1, end, t.begin(), to + 1, function);
-        }
-    }
-    return to + n;
-}
+template <class T, class F>
+T scan_up(T *A , T *L, size_t n, const F& f, T id) {
+    if (n <= 1000) {
+        return scan_inplace_serial(A, n, f, id);
+    } else {
+        T l, r;
 
-template <typename Input, typename Output, typename F, typename Value>
-Output inclusive_scan(Input it, Input end, Output to, F function, Value value) {
-    for (; it != end; ++it, ++to)
-        *to = value = function(value, *it);
-    return to;
-}
+        auto f1 = [&]() { l = scan_up(A, L, n / 2, f, id); };
 
-template <typename Input, typename Tmp, typename F>
-auto scan_up(Input b, Input end, Tmp tmp, F function) {
-    auto n = std::distance(b, end);
-    if (n < MinSize) { return std::accumulate(b + 1, end, *b, function); }
-    else {
-        auto k = n / 2;
-        auto fut = std::async([&]{ tmp[k] = scan_up(b, b + k, tmp, function); });
-        auto right = scan_up(b + k, end, tmp + k, function);
-        fut.wait();
-        return function(tmp[k], right);
+        auto f2 = [&]() { r = scan_up(A + n / 2, L + n / 2 , n - n / 2, f, id); };
+
+        par_do(f1, f2);
+
+        L[n / 2 - 1] = l;
+
+        return f(l, r);
     }
 }
 
-template <typename V, typename Input, typename T, typename To, typename F>
-void scan_down(V v, Input b, Input end, T tmp, To to, F function) {
-    auto n = std::distance(b, end);
-    if (n < MinSize) { inclusive_scan(b, end, to, function, v); }
-    else {
-        auto k = n / 2;
-        auto fut = std::async([&]{ scan_down(v, b, b + k, tmp, to, function); });
-        scan_down(function(v, tmp[k]), b + k, end, tmp + k, to + k, function);
-        fut.wait();
+template <class T, class F>
+void scan_down(T *A, T *L, size_t n, const F& f, T s) {
+    if (n <= 1000) {
+        scan_inplace_serial(A, n, f, s);
+    } else {
+        auto f1 = [&]() { scan_down(A, L, n / 2, f, s); };
+        auto f2 = [&]() { scan_down(A + n / 2, L + n / 2 , n - n / 2, f, f(s, L[n / 2 - 1])); };
+        par_do(f1, f2);
     }
 }
+
+template <class T, class F>
+T scan_inplace(T *A, size_t n, const F& f, T id) {
+
+    T* L = (T*)malloc((n-1) * sizeof(T));
+
+    T sum = scan_up(A, L, n, f, id);
+    scan_down(A, L, n, f, id);
+
+    free(L);
+
+    return sum;
+}
+
+
 
 
 
